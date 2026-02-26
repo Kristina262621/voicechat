730dbe373c7d46790994c3381be6fcd5d14ff6c1
Kristina262621 <yemelina_1997@rambler.ru>
2026-02-26 22:53:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Voice Chat Server работает!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+let activeUsers = new Set();
+
 io.on('connection', (socket) => {
-  console.log('Пользователь подключился:', socket.id);
+  console.log('Подключился:', socket.id);
+
+  socket.on('join', () => {
+    activeUsers.add(socket.id);
+    io.emit('user-count', activeUsers.size);
+    console.log('Вошёл в чат:', socket.id);
+  });
+
+  socket.on('leave', () => {
+    activeUsers.delete(socket.id);
+    io.emit('user-count', activeUsers.size);
+    console.log('Вышел из чата:', socket.id);
+  });
 
   socket.on('disconnect', () => {
-    console.log('Пользователь отключился:', socket.id);
+    activeUsers.delete(socket.id);
+    io.emit('user-count', activeUsers.size);
+    console.log('Отключился:', socket.id);
   });
 });
 
-http.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
