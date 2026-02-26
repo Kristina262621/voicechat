89ab4d2b9438f53c54f655ad515d8ad0506032ac
Kristina262621 <yemelina_1997@rambler.ru>
2026-02-27 01:55:11 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -617,11 +617,27 @@ function playBeep(type) {
 // ═══════════════════════════════════════════════
 const iceServers = {
   iceServers: [
-    { urls:'stun:stun.l.google.com:19302'  },
-    { urls:'stun:stun1.l.google.com:19302' },
-    { urls:'turn:openrelay.metered.ca:80',    username:'openrelayproject', credential:'openrelayproject' },
-    { urls:'turn:openrelay.metered.ca:443',   username:'openrelayproject', credential:'openrelayproject' },
-    { urls:'turn:openrelay.metered.ca:443?transport=tcp', username:'openrelayproject', credential:'openrelayproject' },
+    { urls: 'stun:stun.relay.metered.ca:80' },
+    {
+      urls:       'turn:global.relay.metered.ca:80',
+      username:   '4219a9030e911d3a21936639',
+      credential: 'W9K/4EBqUUoxu9FC'
+    },
+    {
+      urls:       'turn:global.relay.metered.ca:80?transport=tcp',
+      username:   '4219a9030e911d3a21936639',
+      credential: 'W9K/4EBqUUoxu9FC'
+    },
+    {
+      urls:       'turn:global.relay.metered.ca:443',
+      username:   '4219a9030e911d3a21936639',
+      credential: 'W9K/4EBqUUoxu9FC'
+    },
+    {
+      urls:       'turns:global.relay.metered.ca:443?transport=tcp',
+      username:   '4219a9030e911d3a21936639',
+      credential: 'W9K/4EBqUUoxu9FC'
+    },
   ]
 };
 
